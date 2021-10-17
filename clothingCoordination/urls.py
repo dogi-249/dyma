79a6b87b19695c00079b709d89704f39a037215a
onlyeatsapple <90950515+onlyeatsapple@users.noreply.github.com>
2021-10-17 14:21:54 +0900

    Update urls.py

--- clothingCoordination/urls.py
+++ clothingCoordination/urls.py
@@ -1,7 +1,19 @@
+#project/blog/urls.py
+
 from django.urls import path
-from clothingCoordination import views
+from . import views
+
 
 app_name = 'clothing'
+
 urlpatterns = [
-  path('test/', views.test),
+        path('', views.IndexView.as_view(), name='index'),
+        path('category/<str:category>/', views.CategoryView.as_view(),
+                                       name='category'),
+        path('tag/<str:tag>/', views.TagView.as_view(),
+                                       name='tag'),
+        # path('', views.index, name='index'),
+        # path('category/<str:category>/', views.category, name='category'),
+        # path('tag/<str:tag>/', views.tag, name='tag'),
+        path('detail/<int:blog_id>/', views.detail, name='detail'),
 ]
