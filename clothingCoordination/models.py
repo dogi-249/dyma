100532cb78025ea76aaed773be41b63b7bef2f6b
onlyeatsapple <90950515+onlyeatsapple@users.noreply.github.com>
2021-10-17 14:17:36 +0900

    Update models.py

--- clothingCoordination/models.py
+++ clothingCoordination/models.py
@@ -1,3 +1,75 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField('タイトル', max_length=50)
+    text = models.TextField('テキスト')
+    created_at = models.DateField('作成日', auto_now_add=True)
+    updated_at = models.DateField('更新日', auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'ブログ'
+        verbose_name_plural = 'ブログ'
+
+from django.db import models
+
+
+""" カテゴリーモデル """
+class Category(models.Model):
+    name = models.CharField('カテゴリー', max_length=50)
+
+    def __str__(self):
+        return self.name
+
+""" Blogモデル """122
+class Blog(models.Model):
+    title = models.CharField('タイトル', max_length=50)
+    text = models.TextField('テキスト')
+
+    """ プラスアルファ """
+    category = models.ForeignKey(
+                    Category, verbose_name='カテゴリー',
+                    on_delete=models.PROTECT
+               )
+    created_at = models.DateField('作成日', auto_now_add=True)
+    updated_at = models.DateField('更新日', auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'ブログ'
+        verbose_name_plural = 'ブログ'
+
+    """ Tagモデル"""
+class Tag(models.Model):
+    name = models.CharField('タグ', max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Blog(models.Model):
+    title = models.CharField('タイトル', max_length=50)
+    text = models.TextField('テキスト')
+    category = models.ForeignKey(
+                    Category, verbose_name='カテゴリー',
+                    on_delete=models.PROTECT
+               )
+
+    """ Tagモデルと紐づけ """
+    tag = models.ManyToManyField(Tag, verbose_name='タグ')
+
+    """ Blogモデル内の要素と紐づけ """
+    relation = models.ManyToManyField('self', verbose_name='関連', blank=True, null=True)
+    created_at = models.DateField('作成日', auto_now_add=True)
+    updated_at = models.DateField('更新日', auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'ブログ'
+        verbose_name_plural = 'ブログ'
